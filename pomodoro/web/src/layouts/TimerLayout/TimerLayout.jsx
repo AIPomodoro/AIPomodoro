91ba8a49c9192d0728c167f8a5537b83ca6fec72
danielmallett <dmallett@tennessee.edu>
2024-03-12 13:46:54 -0400

    update user email in db

--- pomodoro/web/src/layouts/TimerLayout/TimerLayout.jsx
+++ pomodoro/web/src/layouts/TimerLayout/TimerLayout.jsx
@@ -1,7 +1,39 @@
-import { Link, routes } from '@redwoodjs/router'
+import { useEffect } from 'react'
 
+import { useMutation } from '@redwoodjs/web'
+
+import { useAuth } from 'src/auth'
 import Navbar from 'src/components/Navbar'
+
+const UPDATE_PROFILE = gql`
+  mutation UpdateProfileMutation($id: Int!, $input: UpdateProfileInput!) {
+    updateProfile(id: $id, input: $input) {
+      email
+    }
+  }
+`
+
 const TimerLayout = ({ children }) => {
+  const { isAuthenticated, currentUser, userMetadata } = useAuth()
+  const [update] = useMutation(UPDATE_PROFILE)
+
+  //update user email
+  useEffect(() => {
+    if (
+      currentUser?.profile?.email == null &&
+      currentUser?.profile?.id &&
+      userMetadata?.email &&
+      isAuthenticated
+    ) {
+      update({
+        variables: {
+          id: currentUser?.profile?.id,
+          input: { email: userMetadata?.email },
+        },
+      })
+    }
+  }, [currentUser, isAuthenticated, userMetadata, update])
+
   return (
     <>
       <Navbar />
