0b40b9b0ed818e9937d8cb9d6b7d0b7dd2740b81
Daniel <dmallett@tennessee.edu>
2024-04-04 14:43:45 -0400

    merge hell

--- pomodoro/web/src/components/Navbar/Navbar.jsx
+++ pomodoro/web/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { useTimerContext } from 'src/providers/contexts/TimerContext'
 
 import SettingsModal from '../SettingsModal/SettingsModal'
 
-const Navbar = () => {
+const Navbar = ({ onJournalButtonClick }) => {
   const { isAuthenticated, logIn, logOut } = useAuth()
 
   const { isSettingsOpen, setIsSettingsOpen, isNavMenuOpen, setIsNavMenuOpen } =
@@ -37,7 +37,10 @@ const Navbar = () => {
                 </button>
                 {isAuthenticated ? (
                   <>
-                    <button className="h-10 w-20 rounded-md p-2 transition-all duration-150 ease-in-out hover:bg-gray-500">
+                    <button
+                      onClick={onJournalButtonClick}
+                      className="h-10 w-20 rounded-md p-2 transition-all duration-150 ease-in-out hover:bg-gray-500"
+                    >
                       Journal
                     </button>
                     {/* TODO implement profile page */}
@@ -131,7 +134,7 @@ const Navbar = () => {
                 {isAuthenticated ? (
                   <>
                     <li>
-                      <button>Journal</button>
+                      <button onClick={onJournalButtonClick}>Journal</button>
                     </li>
                     <li>
                       <Link to={routes.dash()}>Profile</Link>
